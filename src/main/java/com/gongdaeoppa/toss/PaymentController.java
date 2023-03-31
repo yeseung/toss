4b1492df7323d1e3511fe88ad904051aacd8b5cb
yeseung <hayeseung@gmail.com>
2023-03-31 10:51:36 +0900

    Initial commit

---
+++ src/main/java/com/gongdaeoppa/toss/PaymentController.java
@@ -0,0 +1,133 @@
+package com.gongdaeoppa.toss;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class PaymentController {
+
+    @Value("${tosspayments.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${tosspayments.key}")
+    private String KEY;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostConstruct
+    private void init() {
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+            }
+        });
+    }
+
+    @RequestMapping("/success")
+    public String confirmPayment(
+            @RequestParam String paymentKey, @RequestParam String orderId, @RequestParam Long amount,
+            Model model) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
+        headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> payloadMap = new HashMap<>();
+
+        //select //별별별별별별별별별별별별별별별별별별별별별별별별별별
+        payloadMap.put("orderId", orderId);
+        payloadMap.put("amount", String.valueOf(amount));
+
+        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
+                "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            JsonNode successNode = responseEntity.getBody();
+            model.addAttribute("orderId", successNode.get("orderId").asText());
+            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
+            return "success";
+        } else {
+            JsonNode failNode = responseEntity.getBody();
+            model.addAttribute("message", failNode.get("message").asText());
+            model.addAttribute("code", failNode.get("code").asText());
+            return "fail";
+        }
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("tosspayments_key", KEY);
+        System.out.println("model = " + model);
+        return "home";
+    }
+
+    @RequestMapping("/fail")
+    public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("code", code);
+        return "fail";
+    }
+
+    @RequestMapping("/virtual-account/callback")
+    @ResponseStatus(HttpStatus.OK)
+    public void handleVirtualAccountCallback(@RequestBody CallbackPayload payload) {
+        if (payload.getStatus().equals("DONE")) {
+            // handle deposit result
+        }
+    }
+
+    private static class CallbackPayload {
+        public CallbackPayload() {}
+
+        private String secret;
+        private String status;
+        private String orderId;
+
+        public String getSecret() {
+            return secret;
+        }
+
+        public void setSecret(String secret) {
+            this.secret = secret;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getOrderId() {
+            return orderId;
+        }
+
+        public void setOrderId(String orderId) {
+            this.orderId = orderId;
+        }
+    }
+}
---
+++ src/test/java/com/gongdaeoppa/toss/TossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongdaeoppa.toss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
